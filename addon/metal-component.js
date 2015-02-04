e1bf6810fe2022294c4bf4eb1b255179074fe7e5
Erik Bryn <erik.bryn@gmail.com>
2015-02-04 10:43:49 -0800

    Fix MetalComponent context

--- addon/metal-component.js
+++ addon/metal-component.js
@@ -7,6 +7,17 @@ var MetalComponent = MetalView.extend();
 var MethodsFromEmberComponent = {
   _yield: Ember.Component.proto()._yield
 };
+
+var ComponentMixin = Ember.Mixin.create({
+  init: function(props) {
+    props.context = this;
+    props.controller = this;
+    this._super(props);
+    this._keywords.view = this;
+  }
+});
+
+ComponentMixin.apply(MetalComponent.prototype);
 extend(MetalComponent.prototype, MethodsFromEmberComponent);
 
 export default MetalComponent;
